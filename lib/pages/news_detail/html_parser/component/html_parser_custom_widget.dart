b41a5eaf4dbcbc6ef2c68d83efd42ef1de3bc165
Do Van Thuc <thucdv@funtap.vn>
2022-09-23 10:06:15 +0700

    update html parser versison

--- lib/pages/news_detail/html_parser/component/html_parser_custom_widget.dart
+++ lib/pages/news_detail/html_parser/component/html_parser_custom_widget.dart
@@ -7,6 +7,8 @@ import 'package:flutter_html/flutter_html.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import '../html_parser_shared.dart';
 import '../html_parser_widget.dart';
+import 'html_custom_render_widget.dart';
+import 'package:flutter_html_all/flutter_html_all.dart';
 
 class HtmlParserCustomWidget extends HtmlParserWidgetBase {
   const HtmlParserCustomWidget({required HtmlParserElement element})
@@ -20,43 +22,18 @@ class HtmlParserCustomWidget extends HtmlParserWidgetBase {
       child: Html(
         data: element.content,
         shrinkWrap: false,
-         customRender: {
-          "iframe": ((context, parsedChild) { 
-            String? url = context.tree.attributes["src"]; 
-              double? givenHeight = null ;//double.tryParse(context.tree.element?.attributes['height'] ?? "");
-  
-            if (url == null) {
-              return SIZED_BOX_H02;
-            }
-
-            return SizedBox(
-              width: MediaQuery.of(context.buildContext).size.width,
-              height: givenHeight ??  (MediaQuery.of(context.buildContext).size.width *  9/ 16), child: WebView(
-        initialUrl: url,
-        key: key,
-        javascriptMode: JavascriptMode.unrestricted,
- 
-        gestureRecognizers: {
-          Factory<VerticalDragGestureRecognizer>(() => VerticalDragGestureRecognizer())
-        },
-      ));
-          })
-        },
-        customImageRenders:  {
-          networkSourceMatcher(): (context, attributes, element) {
-            return ClipRRect(
-              borderRadius:  BorderRadius.circular(10),
-              child:  CachedNetworkImage( 
-                      fit: BoxFit.cover,
-                      imageUrl: attributes["src"] ??"",
-                      placeholder: (context, url) => Transform.scale(
-                        scale: 0.5,
-                        child: const SizedBox(width: 18, height: 18, child:   CircularProgressIndicator()),
-                      ),
-                      errorWidget: (context, url, error) => Icon(Icons.error),
-                    ),);
-          }
-        },
+         customRenders: {
+          iframeCustomMatcher() :  iframeCustomRender(),
+          networkSourceMatcher(): networkImageCustomRender(),
+          tableMatcher(): tableRender(),
+          iframeMatcher(): CustomIframeRender(),
+          svgTagMatcher(): svgTagRender(),
+          svgDataUriMatcher(): svgDataImageRender(),
+          svgAssetUriMatcher(): svgAssetImageRender(),
+          svgNetworkSourceMatcher(): svgNetworkImageRender(),
+          videoMatcher(): videoRender(),
+          audioMatcher(): audioRender(),  
+        }, 
         style: {
           "img": Style( 
             width: MediaQuery.of(context).size.width,
